1bf0699c8e3d9f7a307c3f0bb753a9d722a50329
Kushal Lalwani <109090706+kushal-lalwani@users.noreply.github.com>
2023-07-28 20:14:27 +0000

    Completed Day - 16 tasks

---
+++ Day-16/main.js
@@ -0,0 +1,135 @@
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`
+
+const txt2 = JSON.stringify(skills);
+console.log(txt2);
+
+const JSON_age = JSON.stringify(age);
+console.log(JSON_age);
+
+const isMarried_JSON = JSON.stringify(isMarried);
+console.log(isMarried_JSON);
+
+const student_JSON = JSON.stringify(student,undefined,4);
+console.log(student_JSON);
+
+// LEVEL - 2
+
+const student_JSON2 = JSON.stringify(student,[`firstName`,`lastName`,`skills`]);
+console.log(student_JSON2);
+
+// Level - 3
+
+const txtOBJ = JSON.parse(txt);
+console.log(txtOBJ);
+
+for(const user in txtOBJ){
+    if(txtOBJ[user].skills.length > 4){
+        console.log(txtOBJ[user]);
+    }
+}
+
